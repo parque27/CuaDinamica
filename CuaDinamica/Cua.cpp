b97ff1e75f3ec501b5d2917556e79e21d2067db0
parque <pauarqueros27@gmail.com>
2022-04-19 20:40:03 +0200

    Implementada Cua.cpp

--- CuaDinamica/Cua.cpp
+++ CuaDinamica/Cua.cpp
@@ -8,3 +8,74 @@ Cua::Cua()
 {
 	a_primera = a_ultima = NULL;
 }
+// CONSTRUCTOR COPIA
+Cua::Cua(const Cua& o)
+{
+	a_primera = a_ultima = NULL;
+	copia(o);
+}
+// DESTRUCTOR
+Cua::~Cua()
+{
+	allibera();
+}
+// OPERADORS
+Cua& Cua::operator=(const Cua& o)
+{
+	if (this != &o)
+	{
+		allibera();
+		copia(o);
+	}
+	return *this;
+}
+// METODES CONSULTORS
+bool Cua::es_buida() const
+{
+	return a_primera == NULL;
+}
+Fitxa Cua::primera() const
+{
+	return a_primera->f;
+}
+// METODES MODIFICADORS
+void Cua::encua(Fitxa fitxa)
+{
+	Node* nou = new Node;
+	nou->f = fitxa;
+	nou->seg = NULL;
+	if (es_buida()) 
+		a_primera = a_ultima = nou;
+	else
+	{
+		a_ultima->seg = nou;
+		a_ultima = nou;
+	}
+}
+void Cua::desencua()
+{
+	Node* aux = a_primera;
+	if (a_primera == a_ultima) a_primera = a_ultima = NULL;
+	else a_primera = a_primera->seg;
+	delete aux;
+}
+// METODES D'INSTNACIA
+void Cua::copia(const Cua& o)
+{
+	Node* p = o.a_primera;
+	while (p != NULL)
+	{
+		encua(p->f);
+		p = p->seg;
+	}
+}
+void Cua::allibera()
+{
+	while (a_primera != NULL)
+	{
+		Node* aux = a_primera;
+		a_primera = a_primera->seg;
+		delete aux;
+	}
+	a_ultima = NULL;
+}
