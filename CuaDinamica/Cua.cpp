7923fed350e537deef04764e42e420366fb489d6
parque <pauarqueros27@gmail.com>
2022-04-19 20:24:00 +0200

    Afegits pre i post a Cua.h

--- CuaDinamica/Cua.cpp
+++ CuaDinamica/Cua.cpp
@@ -1,3 +1,10 @@
 #include "Cua.h"
 #include <iostream>
 using namespace std;
+
+// CONSTRUCTORS
+// CONSTRUCTOR PER DEFECTE
+Cua::Cua()
+{
+	a_primera = a_ultima = NULL;
+}
--- CuaDinamica/Cua.h
+++ CuaDinamica/Cua.h
@@ -9,21 +9,29 @@ class Cua
 	public:
 		// CONSTRUCTORS
 		Cua();
+		// Pre: --; Post: cua buida
 		Cua(const Cua& o);
+		// Pre: --; Post: aquesta cua es copia de la cua o
 
 		// DESTRUCTOR
 		~Cua();
-
+		// Pre: --; Post: memoria alliberada
+		
 		// OPERADORS
 		Cua& operator=(const Cua& o);
+		// Pre: --; Post: aquesta cua es copia de o
 
 		// METODES CONSULTORS
 		bool es_buida() const;
+		// Pre: --; Post: retorna true si la cua es buida, fals altrament
 		Fitxa primera() const;
+		// Pre: cua no buida; Post: retorna una copia del primer element de la cua
 
 		// METODES MODIFICADORS
 		void encua(Fitxa f);
+		// Pre: --; Post: ha afegit la fitxa f al final de la cua
 		void desencua();
+		// Pre: cua no buida; Post: ha eliminat el primer element de la cua
 
 	private:
 		// ESTRUCTURA DE DADES
@@ -39,6 +47,8 @@ class Cua
 
 		// METODES D'INSTANCIA
 		void copia(const Cua& o);
+		// Pre: cua buida; Post: aquesta cua és copia de o
 		void allibera();
+		// Pre: --; Post: cua buida
 };
 #endif // !CUA_H
