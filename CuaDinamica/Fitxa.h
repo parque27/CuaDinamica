4beda0a09af785092e5362804dc9f9e0649f650f
parque <pauarqueros27@gmail.com>
2022-04-19 22:24:19 +0200

    Afegits comentaris a Fitxa.h

--- CuaDinamica/Fitxa.h
+++ CuaDinamica/Fitxa.h
@@ -7,19 +7,22 @@ class Fitxa
 	public:
 		// CONSTRUCTORS
 		Fitxa();
+		// Pre: --; Post: la fitxa te tipus buit i valor 0
 		Fitxa(char tipus, int valor);
-
+		// Pre: --; Post: la fitxa te els parametres passats per constructor
+	
 		// METODES CONSULTORS
 		void mostra() const;
-
+		// Pre: --: Post: mostra la fitxa per pantalla
+		
 		// METODES MODIFICADORS
 		void llegeix();
+		// Pre: --; Post: llegeix els atributs de la fitxa per teclat
 
 	private:
 		// ATRIBUTS
 		char a_tipus;
 		int a_valor;
-
 };
 #endif // !FITXA_H
 
