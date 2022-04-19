3895f76a6b5dc9c994c49c918438f3864958f9ca
parque <pauarqueros27@gmail.com>
2022-04-19 19:54:38 +0200

    Implementat menu

--- CuaDinamica/CuaDinamica.cpp
+++ CuaDinamica/CuaDinamica.cpp
@@ -13,16 +13,26 @@ void encua(Cua& c)
 {
 
 }
+void mostrar_menu()
+{
+	cout << endl;
+	cout << "1) Encuar fitxa" << endl;
+	cout << "2) Desencuar fitxa" << endl;
+	cout << "3) Mostrar primera" << endl;
+	cout << "4) Mostrar cua sencera" << endl;
+	cout << "5) Mostrar menu" << endl;
+	cout << "6) Sortir" << endl;
+}
 int main()
 {
 	Cua c;
-
+	mostrar_menu();
 	int opcio = introduir_opcio();
-	while (opcio != 4)
+	while (opcio != 6)
 	{
 		if (opcio == 1) encua(c);
-		else if (opcio == 2)
-		else if (opcio == 3)
+		/*else if (opcio == 2)
+		else if (opcio == 3)*/
 
 		opcio = introduir_opcio();
 	}
