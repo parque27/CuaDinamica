d3b99f227d7eb774c70bfb84735fd054a558bd53
PAU ARQUEROS <pauarqueros27@gmail.com>
2022-04-21 16:24:53 +0200

    Actualitzada classe Fitxa

--- CuaDinamica/Fitxa.cpp
+++ CuaDinamica/Fitxa.cpp
@@ -15,15 +15,28 @@ Fitxa::Fitxa(char element, int valor)
 	a_tipus = element;
 	a_valor = valor;
 }
-
+// OPERADORS
+bool Fitxa::operator<(const Fitxa& f)
+{
+	bool es_menor = a_tipus < f.a_tipus;
+	if (a_tipus == f.a_tipus) es_menor = a_valor < f.a_valor;
+	return es_menor;
+}
 // METODES CONSULTORS
 void Fitxa::mostra() const
 {
-	cout << "(" << a_tipus;
-	if (a_valor <= 9) cout << 0;
-	cout << a_valor << ")";
+	if (this->es_nula()) cout << "(   )" << endl;
+	else
+	{
+		cout << "(" << a_tipus;
+		if (a_valor <= 9) cout << 0;
+		cout << a_valor << ")";
+	}
 }
-
+bool Fitxa::es_nula() const
+{
+	return a_tipus == ' ' and a_valor == 0;
+}
 // METODES MODIFICADORS
 void Fitxa::llegeix()
 {
